4845909d01ab24f521f57b9bb8974fd43e342a91
Wesley <wesleykok-@hotmail.com>
2019-10-11 22:13:21 +1000

    First task gets all posts not stickied

--- config.py
+++ config.py
@@ -1,35 +1,11 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-CLIENT = {
-    "ID": os.getenv("REDDIT_ID"),
-    "SECRET": os.getenv("REDDIT_SECRET")
-}
+ID = os.getenv("REDDIT_ID")
+SECRET = os.getenv("REDDIT_SECRET")
+USERNAME = os.getenv("REDDIT_USER")
+PASSWORD = os.getenv("REDDIT_PASSWORD")
 
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD")
-
-USER_AGENT = os.getenv("USER_AGENT")
-
-# Read subreddit from environment variable; warn and default to /r/all if not set
-SUBREDDIT = os.getenv("SUBREDDIT")
-if SUBREDDIT is None:
-    logging.warning("Environment variable `SUBREDDIT` is not set; defaulting to `all`")
-    SUBREDDIT = "all"
-
-COMMENT_LENGTH_LIMIT = 9900
-
-    © 2019 GitHub, Inc.
-    Terms
-    Privacy
-    Security
-    Status
-    Help
-
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
+USER_AGENT = os.getenv("REDDIT_AGENT")
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import praw
+import config
 
-reddit = praw.Reddit(client_id='my client id',
-                     client_secret='my client secret',
-                     user_agent='my user agent')
+reddit = praw.Reddit(client_id=config.ID,
+                     client_secret=config.SECRET,
+                     user_agent=config.USER_AGENT,
+					 username=config.USERNAME,
+					 password=config.PASSWORD)
+					 
+subreddit = reddit.subreddit('singapore')
+
+for submission in subreddit.hot(limit=10):
+	if not submission.stickied:
+	   print(submission.title)
