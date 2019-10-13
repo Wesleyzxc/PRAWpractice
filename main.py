04ffded3c1238949a6cc8abb445f075cbb332b0a
Wesley <wesleykok-@hotmail.com>
2019-10-13 20:19:45 +1000

    translate from specified reddit and submission, needs refactoring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import praw
 import config
+import re
+from googletrans import Translator
 
 reddit = praw.Reddit(client_id=config.ID,
                      client_secret=config.SECRET,
@@ -7,8 +9,26 @@ reddit = praw.Reddit(client_id=config.ID,
 					 username=config.USERNAME,
 					 password=config.PASSWORD)
 					 
-subreddit = reddit.subreddit('singapore')
+subreddit = reddit.subreddit('learnjapanese')
 
+titleID = []
 for submission in subreddit.hot(limit=10):
 	if not submission.stickied:
-	   print(submission.title)
+	   titleID.append(submission.id)
+
+
+translator = Translator()
+thread = reddit.submission(id=titleID[0])
+thread.comments.replace_more(limit=None)
+RE_EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
+for comments in thread.comments.list():
+    comments.body = RE_EMOJI.sub(r'', comments.body)
+    # print(translator.detect(comments.body))
+    if (translator.detect(comments.body).lang!='en'): # Checks for anything non-English
+        print(comments.body)
+        print(translator.translate(comments.body).text)
+    
+    
+    
+if __name__ == "__main__":
+    start()
